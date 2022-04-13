b53d62d515ca3e279e06a2c71cb619903ce1ffcd
Michael Hall <mh28@sanger.ac.uk>
2022-04-13 09:10:48 +0100

    __init__ with some commonly used functions and classes

---
+++ clone_competition_simulation/__init__.py
@@ -0,0 +1,5 @@
+from .parameters import Parameters
+from .fitness_classes import Gene, MutationGenerator
+from .fitness_classes import NormalDist, UniformDist, ExponentialDist, FixedValue
+from .fitness_classes import UnboundedFitness, BoundedLogisticFitness
+from .colourscales import get_CS_random_colours_from_colourmap, ColourScale
